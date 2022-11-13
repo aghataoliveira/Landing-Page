b1ff9be01bda348313840b016d4e7f47ab1fd11b
aghataoliveira <aghatagoncalves@outlook.com>
2022-11-12 22:01:48 -0500

    javascript practice before project

--- js/app.js
+++ js/app.js
@@ -3,3 +3,69 @@ function myFunction() {
 alert("I am an alert box!");
 }
 
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 10;
+var checkBalance = true;
+var isActive = true;
+
+// your code goes here
+// check your balance?? false
+
+// if (checkBalance == false){ 
+//     console.log ("Thank you. Have a nice day!");
+// } else if (checkBalance == true){ // check your balance?? true
+//     if (isActive == true && balance > 0){
+//         console.log ("Your balance is $" + balance + ".");
+//     } else if (isActive == true && balance == 0){ 
+//     console.log ("Your account is empty.");
+//     } else if (isActive == true && balance < 0){ 
+//         console.log ("Your balance is negative. Please contact bank.");
+//         }
+// } else{
+//     console.log ("Your account is no longer active.");
+// }
+// // 
+// if ((flavor === "vanilla" || "chocolate") && (vessel === "cone" || "bowl") && (toppings === "sprinkles"|| "peanut")){
+//     console.log ("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings +".")
+// }
+// if ((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")){
+//     console.log ("I'd like two scoops of " + flavor + "ice cream in a " + vessel + "with " + toppings + ".")
+// }
+
+// // 
+// if ((shirtWidth >= 18 || shirtWidth <= 19.99) && shirtLength == 28 && (shirtSleeve = 8.13 || shirtSleeve >= 8.37)){
+//     console.log ("P");
+// } else if ((shirtWidth >= 20 || shirtWidth <= 21.99) && shirtLength == 29 && (shirtSleeve >= 8.38 || shirtSleeve >= 8.62)){
+//     console.log ("M");
+// } else if ((shirtWidth >= 22 || shirtWidth <= 23.99) && shirtLength == 30 && (shirtSleeve >= 8.63 || shirtSleeve >= 8.87)){
+//     console.log ("L");
+// } else if ((shirtWidth >= 24 || shirtWidth <= 25.99) && (shirtLength == 31 || shirtLenght == 32) && (shirtSleeve >= 8.88 || shirtSleeve >= 9.62)){
+//     console.log ("XL");
+// } else if ((shirtWidth >= 26 || shirtWidth <= 27.99) && shirtLength == 33 && (shirtSleeve >= 9.63 || shirtSleeve >= 10.12)){
+//     console.log ("2XL");
+// } else if (shirtWidth == 28 && shirtLength == 34 && shirtSleeve == 10.13){
+//     console.log ("3XL");
+// } else {
+//     console.log ("NA");
+// }
+// // AQUI TA A SOLUCAO CORRETA
+// var shirtWidth = 28;
+// var shirtLength = 34;
+// var shirtSleeve = 10.13;
+
+// // WRITE YOUR CODE HERE
+// if ((shirtWidth >= 18 && shirtWidth <= 19.99) && (shirtLength >= 28 && shirtLength <= 28.99) && (shirtSleeve >= 8.13 && shirtSleeve <= 8.379)){
+//     console.log ("S");
+// } else if ((shirtWidth >= 20 && shirtWidth <= 21.99) && (shirtLength >= 29 && shirtLength <= 29.99) && (shirtSleeve >= 8.38 && shirtSleeve <= 8.629)){
+//     console.log ("M");
+// } else if ((shirtWidth >= 22 && shirtWidth <= 23.99) && (shirtLength >= 30 && shirtLength <= 30.99) && (shirtSleeve >= 8.63 && shirtSleeve <= 8.879)){
+//     console.log ("L");
+// } else if ((shirtWidth >= 24 && shirtWidth <= 25.99) && (shirtLength >= 31 && shirtLength <= 32.99) && (shirtSleeve >= 8.88 && shirtSleeve <= 9.629)){
+//     console.log ("XL");
+// } else if ((shirtWidth >= 26 && shirtWidth <= 27.99) && (shirtLength >= 33 && shirtLength <= 33.99) && (shirtSleeve >= 9.63 && shirtSleeve <= 10.129)){
+//     console.log ("2XL");
+// } else if (shirtWidth == 28 && shirtLength == 34 && shirtSleeve == 10.13){
+//     console.log ("3XL");
+// } else {
+//     console.log ("NA");
+// }
