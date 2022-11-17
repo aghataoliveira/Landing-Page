96c32a62fd0f0c876eb331ce6cb5ad5c10af07e4
aghataoliveira <aghatagoncalves@outlook.com>
2022-11-17 13:33:45 -0500

    test: starting ARRAYS

--- js/app.js
+++ js/app.js
@@ -351,3 +351,28 @@
 
 
 //---------------------------------------------------------------ARRAY-----------------------------------------------------------------
+var donutBox = [
+    ["glazed", "chocolate glazed", "cinnamon"],
+    ["powdered", "sprinkled", "glazed cruller"],
+    ["chocolate cruller", "Boston creme", "creme de leche"]
+  ];
+  
+  for (var row = 0; row < donutBox.length; row++) {
+    // here, donutBox[row].length refers to the length of the donut array currently being looped over
+    for (var column = 0; column < donutBox[row].length; column++) {
+      console.log(donutBox[row][column]);
+    }
+  }
+  
+  
+  var bills = [50.23, 19.12, 34.01,
+      100.11, 12.15, 9.90, 29.11, 12.99,
+      10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+  ];
+  
+  var totals = bills.map(function(bill){
+      bill = bill *0.15 + bill;
+      bill = (bill.toFixed(2));
+      return(Number(bill));
+  });
+  console.log(totals);
