11fe05620ec7f26103e8efad32aade905ffe6ad8
aghataoliveira <aghatagoncalves@outlook.com>
2022-12-13 20:40:10 -0500

    feature: section 4 created

--- js/app.js
+++ js/app.js
@@ -23,13 +23,18 @@
  * 
 */
 // Get the elements first
-const NavBar = document.getElementById("navbar__list");
-const Section = document.querySelectorAll("section");
-const Section1 = document.getElementById("section1");
-const Section2 = document.getElementById("section2");
-const Section3 = document.getElementById("section3");
+const navBar = document.getElementById('navbar__list');
+// const navBar = document.querySelector('navbar__list');
+const section = document.querySelectorAll("section");
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
 const Section4 = document.getElementById("section4");
-const NavBarSelect = document.querySelectorAll(".navbar__menu a");
+// const NavBarSelect = document.querySelectorAll(".navbar__menu a");
+const textOfParagraph = document.createTextNode('I am the text for the paragraph!');
+
+navBar.appendChild(textOfParagraph);
+console.log(navBar);
 
 /**
  * End Global Variables
@@ -37,6 +42,47 @@ const NavBarSelect = document.querySelectorAll(".navbar__menu a");
  * 
 */
 
+function createMenu(){
+
+    // All items we'd like to add
+    var navItems = [
+        {href: 'http://google.com', text: 'Google'},
+        {href: 'http://bing.com', text: 'Bing'},
+        {href: 'http://stackoverflow.com', text: 'StackOverflow'}
+    ];
+
+    // A few variables for use later
+    var navElem = document.createElement("nav"),
+        navList = document.createElement("ul"), 
+        navItem, navLink;
+
+    navElem.appendChild(navList);
+    navElem.style.display = 'block';
+    navElem.style.cssText = 'color: blue; background-color: orange; font-size: 3.5em';
+
+for (var i = 0; i < navItems.length; i++) {
+    // Create a fresh list item, and anchor
+    navItem = document.createElement("li");
+    navLink = document.createElement("a");
+
+    // Set properties on anchor
+    navLink.href = navItems[i].href;
+    navLink.innerHTML = navItems[i].text;
+
+    // Add anchor to list item, and list item to list
+    navItem.appendChild(navLink);
+    navList.appendChild(navItem);
+}
+    // Set first list item as current
+    navList.children[0].className = "current";
+
+    // Add list to body (or anywhere else)
+    window.onload = function () {
+        document.body.appendChild(navElem);
+    }
+
+
+};
 
 
 /**
@@ -67,46 +113,50 @@ const NavBarSelect = document.querySelectorAll(".navbar__menu a");
 // Set sections as active
 
 //stackOverflow test
-(function(){
+// (function(){
 
-    // All items we'd like to add
-    var navItems = [
-        {href: 'http://google.com', text: 'Google'},
-        {href: 'http://bing.com', text: 'Bing'},
-        {href: 'http://stackoverflow.com', text: 'StackOverflow'}
-    ];
+//     // All items we'd like to add
+//     var navItems = [
+//         {href: 'http://google.com', text: 'Google'},
+//         {href: 'http://bing.com', text: 'Bing'},
+//         {href: 'http://stackoverflow.com', text: 'StackOverflow'}
+//     ];
 
-    // A few variables for use later
-    var navElem = document.createElement("nav"),
-        navList = document.createElement("ul"), 
-        navItem, navLink;
+//     // A few variables for use later
+//     var navElem = document.createElement("nav"),
+//         navList = document.createElement("ul"), 
+//         navItem, navLink;
 
-    navElem.appendChild(navList);
+//     navElem.appendChild(navList);
+//     navElem.style.display = 'block';
+//     navElem.style.cssText = 'color: blue; background-color: orange; font-size: 3.5em';
 
-    // Cycle over each nav item
-    for (var i = 0; i < navItems.length; i++) {
-        // Create a fresh list item, and anchor
-        navItem = document.createElement("li");
-        navLink = document.createElement("a");
 
-        // Set properties on anchor
-        navLink.href = navItems[i].href;
-        navLink.innerHTML = navItems[i].text;
+//     // Cycle over each nav item
+//     for (var i = 0; i < navItems.length; i++) {
+//         // Create a fresh list item, and anchor
+//         navItem = document.createElement("li");
+//         navLink = document.createElement("a");
 
-        // Add anchor to list item, and list item to list
-        navItem.appendChild(navLink);
-        navList.appendChild(navItem);
-    }
+//         // Set properties on anchor
+//         navLink.href = navItems[i].href;
+//         navLink.innerHTML = navItems[i].text;
 
-    // Set first list item as current
-    navList.children[0].className = "current";
+//         // Add anchor to list item, and list item to list
+//         navItem.appendChild(navLink);
+//         navList.appendChild(navItem);
+//     }
 
-    // Add list to body (or anywhere else)
-    window.onload = function () {
-        document.body.appendChild(navElem);
-    }
+//     // Set first list item as current
+//     navList.children[0].className = "current";
 
-}());
+//     // Add list to body (or anywhere else)
+//     window.onload = function () {
+//         document.body.appendChild(navElem);
+//     }
+
+
+// }());
 
 
 //This line tests if all my content is working for this page
